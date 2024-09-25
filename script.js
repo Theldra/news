33482c10b97e8f57b6f661254e5c072f17aa87cc
Etheldra Akahoho <etheldeak4@gmail.com>
2024-09-25 11:57:01 +0000

    New code to display normal text

--- script.js
+++ script.js
@@ -12,15 +12,24 @@ fetch('https://newsapi.org/v2/everything?q=keyword&apiKey=bb7f953c78034f868bee1d
             articleH1.textContent = article.title;
             articleDiv.appendChild(articleH1);
 
+            // const articleP = document.createElement('p')
+            // articleP.textContent = article.content;
+            // articleDiv.appendChild(articleP);
+
             const articleP = document.createElement('p')
-            articleP.textContent = article.content;
+            articleP.textContent = article.description;
             articleDiv.appendChild(articleP);
+
+            const articleImg = document.createElement('img');
+            articleImg.setAttribute('src', article.urlToImage);
+            articleImg.setAttribute('alt', article.title);
+            articleImg.classList.add('article-img');
+            articleDiv.appendChild(articleImg);
 
             document.querySelector('#articles').appendChild(articleDiv);
 
         }
     })
-
 
 
 
